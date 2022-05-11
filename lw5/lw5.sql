df4ec52f88a8ffe1981326503429aedde579c73d
Alexmiln <alexmiln04@gmail.com>
2022-05-11 21:39:05 +0300

    Сделал правки в lw5

--- lw5/lw5.sql
+++ lw5/lw5.sql
@@ -17,24 +17,22 @@ ALTER TABLE room_in_booking
     ADD CONSTRAINT `room_in_booking_room_id_room_fk` FOREIGN KEY (`id_room`) REFERENCES `room` (`id_room`)
         ON DELETE CASCADE ON UPDATE CASCADE;
 
--- 2. Выдать информацию о клиентах гостиницы “Космос”, проживающих в номерах категории “Люкс” на сегодня
-SELECT c.id_client, c.name, c.phone, rib.checkin_date AS checkin_date, rib.checkout_date AS checkout_date
+-- 2. Выдать информацию о клиентах гостиницы “Космос”, проживающих в номерах категории “Люкс” на 1 апреля 2019
+SELECT c.name, c.phone, rib.checkin_date AS checkin_date, rib.checkout_date AS checkout_date
 FROM client c
     JOIN booking b ON c.id_client = b.id_client
     JOIN room_in_booking rib ON b.id_booking = rib.id_booking
     JOIN room r ON rib.id_room = r.id_room
     JOIN hotel h on r.id_hotel = h.id_hotel
     JOIN room_category rc on r.id_room_category = rc.id_room_category
-WHERE h.name = 'Космос' AND rc.name = 'Люкс' AND CURDATE() - INTERVAL 3 YEAR BETWEEN rib.checkin_date AND rib.checkout_date - INTERVAL 1 DAY;
--- '- INTERVAL 3 YEAR' исключительно для проверки работоспособности запроса
+WHERE h.name = 'Космос' AND rc.name = 'Люкс' AND '2019-04-01' BETWEEN rib.checkin_date AND rib.checkout_date - INTERVAL 1 DAY;
 
 -- 3. Дать список свободных номеров всех гостиниц на 22 апреля
-SELECT r.number AS room_number, h.name AS hotel_name, rc.name AS room_category, r.price AS price FROM room r
+SELECT DISTINCT r.number AS room_number, h.name AS hotel_name, rc.name AS room_category, r.price AS price FROM room r
     JOIN room_in_booking rib on r.id_room = rib.id_room
     JOIN hotel h on r.id_hotel = h.id_hotel
     JOIN room_category rc on r.id_room_category = rc.id_room_category
 WHERE '2019-04-22' NOT BETWEEN rib.checkin_date AND rib.checkout_date - INTERVAL 1 DAY
-GROUP BY room_number, hotel_name, room_category
 ORDER BY hotel_name;
 
 -- 4. Дать количество проживающих в гостинице “Космос” на 23 марта по каждой категории номеров
@@ -74,6 +72,12 @@ START TRANSACTION;
     INSERT booking (id_client, booking_date) VALUES (50, CURDATE());
     INSERT room_in_booking (id_booking, id_room, checkin_date, checkout_date) VALUES ((SELECT MAX(booking.id_booking) FROM booking), 154, '2022-05-09', '2022-05-30');
 COMMIT;
+
+SELECT * FROM room_in_booking
+JOIN booking b on room_in_booking.id_booking = b.id_booking
+JOIN room r on r.id_room = room_in_booking.id_room
+JOIN client c on c.id_client = b.id_client
+WHERE r.id_room = 154;
 
 -- 9. Добавить необходимые индексы для всех таблиц
 ALTER TABLE room
